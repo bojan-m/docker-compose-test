f66bf2cda6de53d9044fa33037994537adb83b16
Bojan M <bojan.markovic212@gmail.com>
2018-09-19 14:43:30 +0200

    tinkering around with dockerfiles and docker-compose.yml

--- function-2/src/app.go
+++ function-2/src/app.go
@@ -8,7 +8,7 @@ import (
 func sayHello(w http.ResponseWriter, r *http.Request) {
   message := r.URL.Path
   message = strings.TrimPrefix(message, "/")
-  message = "Multiply is:" + message
+  message = "teeeeest:" + message
 
   w.Write([]byte(message))
 }
